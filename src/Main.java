f7771571847d0436d03a43e6867a873ef84e2741
TamaraKoval <kovalpsy@mail.ru>
2023-02-04 02:43:57 +0300

    attraction ready

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,40 @@
+import person.Person;
+
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Deque<Person> queue = new ArrayDeque<>(generateClients());
+        System.out.println(queue); // проверим очередь
+
+        while(true) {
+            if (queue.isEmpty()) {
+                System.out.println("Очередь пуста, аттракцион отдыхает");
+                break;
+            } else {
+                Person currentClient = queue.pollFirst();
+                System.out.println(currentClient.getNameSurname() + " прокатился на аттракционе");
+                currentClient.spendTicket();
+                if (currentClient.haveTicket()) {
+                    queue.offerLast(currentClient);
+                }
+            }
+        }
+    }
+
+    public static List<Person> generateClients() {
+        List<Person> filledQueue = new ArrayList<>();
+        Person anna = new Person("Anna", "Brown", 3);
+        Person alex = new Person("Alex", "Palex", 5);
+        Person maria = new Person("Maria", "Volkova", 2);
+        Person natasha = new Person("Natasha", "Popova", 6);
+        Person gleb = new Person("Gleb", "Freeman", 4);
+        filledQueue.add(anna);
+        filledQueue.add(alex);
+        filledQueue.add(maria);
+        filledQueue.add(natasha);
+        filledQueue.add(gleb);
+
+        return filledQueue;
+    }
 }
--- src/person/Person.java
+++ src/person/Person.java
@@ -1,4 +1,34 @@
 package person;
 
 public class Person {
+    private String name;
+    private String surname;
+    private int tickets;
+
+    public Person(String name, String surname, int tickets) {
+        this.name = name;
+        this.surname = surname;
+        this.tickets = tickets;
+    }
+
+    public int spendTicket() {
+        return tickets -= 1;
+    }
+
+    public boolean haveTicket() {
+        return tickets > 0;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + ": " + tickets;
+    }
+
+    public String getNameSurname() {
+        return name + " " + surname;
+    }
+
+    public int getTickets() {
+        return tickets;
+    }
 }
